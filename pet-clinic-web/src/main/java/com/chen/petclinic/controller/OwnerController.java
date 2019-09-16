3a35a5e465a06cce0cd647b248e9c8b5444c7506
Chen <cheneus@gmail.com>
2019-09-15 20:17:43 -0500

    add owner and vet controller closes #6

---
+++ pet-clinic-web/src/main/java/com/chen/petclinic/controller/OwnerController.java
@@ -0,0 +1,16 @@
+package com.chen.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owner")
+public class OwnerController {
+
+    @RequestMapping({"/", "", "/all"})
+    public String listVets() {
+        return "owners/index";
+    }
+}
