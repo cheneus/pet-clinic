7d6b0659d5ce2365cd9fac1d5f1e4778010094c5
Chen <cheneus@gmail.com>
2019-10-09 21:19:56 -0500

    added layout for vets and owners #17

--- pet-clinic-web/src/main/java/com/chen/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/chen/petclinic/controller/VetController.java
@@ -1,14 +1,25 @@
 package com.chen.petclinic.controller;
 
+import com.chen.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/", "", "/all"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
